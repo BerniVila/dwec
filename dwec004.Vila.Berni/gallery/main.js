70773c88bbb490b89d2340a31a6185da7dd9529b
BerniVila <bernivila.web@gmail.com>
2021-11-02 15:52:52 +0100

    task-finished

--- dwec004.Vila.Berni/gallery/main.js
+++ dwec004.Vila.Berni/gallery/main.js
@@ -6,8 +6,32 @@ const overlay = document.querySelector('.overlay');
 
 /* Looping through images */
 
-const newImage = document.createElement('img');
-newImage.setAttribute('src', xxx);
-thumbBar.appendChild(newImage);
+for (let i = 1; i <= 5; i++) {
+  const newImage = document.createElement('img');
+  newImage.setAttribute('src', `images/pic${i}.jpg`);
+
+  thumbBar.appendChild(newImage);
+}
+
+function changePic(e) {
+  let newImgSrc = e.target.getAttribute('src');
+  displayedImage.setAttribute('src', newImgSrc);
+}
+
+thumbBar.addEventListener('click', changePic);
 
 /* Wiring up the Darken/Lighten button */
+
+btn.onclick = function () {
+  let btnClass = btn.getAttribute('class');
+
+  if (btnClass === 'dark') {
+    btn.setAttribute('class', 'light');
+    btn.textContent = 'Lighten';
+    overlay.style.backgroundColor = 'rgba(0,0,0,0.5)';
+  } else {
+    btn.setAttribute('class', 'dark');
+    btn.textContent = 'Darken';
+    overlay.style.backgroundColor = 'rgba(0,0,0,0)';
+  }
+};
